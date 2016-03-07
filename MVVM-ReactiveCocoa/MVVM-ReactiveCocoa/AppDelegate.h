c532ea2a101b278c71a2f6548e55d30c404bf62c
阳君 <juny1@jumei.com>
2016-03-07 17:52:49 +0800

    使用ReactiveCocoa优化MVVM框架

---
+++ MVVM-ReactiveCocoa/MVVM-ReactiveCocoa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM-ReactiveCocoa
+//
+//  Created by admin on 16/3/7.
+//  Copyright © 2016年 阳君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
